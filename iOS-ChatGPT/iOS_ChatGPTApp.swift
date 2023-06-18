7032824d2da21d233dc0ba10e03df8d1446521af
Vivek Patil <vivekptl892@gmail.com>
2023-06-18 14:24:37 +0530

    Initial project setup

---
+++ iOS-ChatGPT/iOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTApp.swift
+//  iOS-ChatGPT
+//
+//  Created by Patil, Vivek on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS-ChatGPT/macOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_ChatGPTApp.swift
+//  macOS-ChatGPT
+//
+//  Created by Patil, Vivek on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct macOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
